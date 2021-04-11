a1df64865b758829b0249751b40d9551a7d0f294
Prince <70280604+prince2489@users.noreply.github.com>
2021-04-11 20:02:06 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,76 @@
+# from flask import Flask, render_template, url_for
+# app = Flask(__name__)
+# print(__name__)
+
+# @app.route('/<username>/<int:post_id>')
+# def hello_world(username=None, post_id=None):
+#     #print(url_for('static', filename='bolt.ico'))
+#     return render_template('./index.html',name=username,post_id=post_id)
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('./about.html')
+
+# @app.route('/blog')
+# def blog():
+#     return 'HThese are my blog!'
+
+# @app.route('/favicon.ic0')
+# def favicon():
+#     return 'HThese are my blog!'
+
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        file=database.write(f"\n{email},{subject},{message}")
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        csv_writer=csv.writer(database2, delimiter=",",quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # csv_writer = csv.writer(database2, delim)
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data= request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong!'
+
+# @app.route('/works.html')
+# def work():
+#     return render_template('works.html')
+
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/favicon.ic0')
+# def favicon():
+#     return 'HThese are my blog!'
+
